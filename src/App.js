04f7d6bb33bbfe5bd142ea08c93aa2c474c19bea
unknown <vedican.v44@gmail.com>
2023-09-19 23:42:32 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Apple from './components/Apple';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Signup/> */}
+     {/* <Login/> */}
+     <Apple/>
+    </div>
+  );
+}
+
+export default App;
